2caa251c6847cec4d99eb675b484eeff2ab4fa86
Gaurav Ranjan <gauravranjan@outlook.com>
2018-08-25 14:26:54 +0530

    Adding more methods

--- arrays/linkedList.js
+++ arrays/linkedList.js
@@ -4,15 +4,21 @@ export default class LinkedList {
   constructor() {
     this.head = null;
     this.tail = null;
+    if (typeof dataList === 'Array' && dataList.length) {
+      dataList.map((data) => new LinkedListNode(data))
+    }
+  }
+
+  createListFromArray(dataList = []) {
+    dataList.forEach((data) => this.addNodeToHead(data));
+    return this;
   }
 
   addNodeToHead(data) {
     const newNode = new LinkedListNode(data, this.head);
     this.head = newNode;
-
     if (!this.tail)
       this.tail = newNode;
-
     return this;
   }
 
